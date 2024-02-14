a2c0846a3fd5683fab07ad2b63ada9d3171b85a9
guuizzo <guuizzo@hotmail.com>
2024-02-14 10:00:53 -0300

    first commit

---
+++ 05-tests/01/service.js
@@ -0,0 +1,17 @@
+const axios = require('axios')
+const URL = `https://swapi.dev/api/people`
+
+async function obterPessoas(nome) {
+    const url = `${URL}/?search=${nome}&format=json`
+    const response = await axios.get(url)
+    return response.data.results.map((item) => {
+        return {
+            nome: item.name,
+            peso: item.height
+        }
+    })
+}
+
+module.exports = {
+    obterPessoas
+}
